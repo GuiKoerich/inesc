c64e379540b721bd08de496b68fdae12729cd27d
Guilherme <guilherme@pwx.cloud>
2021-03-08 22:48:23 -0300

    Fix interval default value

--- export.py
+++ export.py
@@ -16,7 +16,7 @@ def check_dir(path: str):
 @option('--json', 'json', is_flag=True, help="Cria um arquivo no formato JSON.")
 @option('--collection', 'collection', type=str, help="Collection específica do MongoDB a ser exportada. "
                                                      "Para buscar todas não utilize esta opção")
-@option('--interval', 'interval', type=int,
+@option('--interval', 'interval', type=int, default=2, show_default=True,
         help="O tempo do intervalo em minutos que será considerado na Query. O todos os registros")
 @option('--path', 'file_path', type=str, help="Caminho onde o(s) arquivo(s) serão salvos, caso não exista, "
                                               "ele será criado.")
@@ -25,12 +25,7 @@ def check_dir(path: str):
 def export(csv, json, collection, interval, file_path, between):
     if not csv and not json:
         printer(message=f'É preciso selecionar ao menos um tipo de saída!\nExemplo: export --csv --interval=2 '
-                        f'--path=/home/usuario/exports', status='info')
-        return
-
-    if not interval and not between:
-        printer(message=f'É preciso selecionar ao menos um tipo de intervalo!\nUse o --help para auxiliar!'
-                        f'\npython3 export.py --help',
+                        f'--path=/home/usuario/exports\nUse o --help para auxiliar!\npython3 export.py --help',
                 status='info')
         return
 
